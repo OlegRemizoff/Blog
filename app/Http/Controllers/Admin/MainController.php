49362673dde7d25e7c043d2d56d02a6ad116378a
oleg_remizov <job.rem1zoff@gmail.com>
2024-11-24 16:17:42 +0300

    count post&categories admin.index

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -5,17 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use App\Models\Tag;
+use App\Models\Post;
+use App\Models\Category;
 
 class MainController extends Controller
 {
 
 public function index() {
+    $post_count = Post::count('id');
+    $category_count = Category::count('id');
 
-    // $tag = new Tag();
-    // $tag->title = 'live';
-    // $tag->save();
-    return view('admin.index');
+    return view('admin.index', compact('post_count', 'category_count'));
 }
 
 
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -22,31 +22,60 @@
 
 <!-- Main content -->
 <section class="content">
+  <div class="row">
+    <!-- Posts -->
+    <div class="col-lg-3 col-6">
+      <div class="small-box bg-info">
+        <div class="inner">
+          <h3>{{ $post_count }}</h3>
 
-  <!-- Default box -->
-  <div class="card">
-    <div class="card-header">
-      <h3 class="card-title">Title</h3>
-
-      <div class="card-tools">
-        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-          <i class="fas fa-minus"></i>
-        </button>
-        <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-          <i class="fas fa-times"></i>
-        </button>
+          <p>Статьи</p>
+        </div>
+        <div class="icon">
+          <i class="fas fa-book-open"></i>
+        </div>
+        <a href="{{ route('admin.posts.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
-    <div class="card-body">
-      Start creating your amazing application!
+    <!-- ./end posts -->
+
+    <!-- Categories -->
+    <div class="col-lg-3 col-6">
+      <div class="small-box bg-danger">
+        <div class="inner">
+          <h3>{{ $category_count }}</h3>
+
+          <p>Категории</p>
+        </div>
+        <div class="icon">
+          <i class="fas fa-tasks"></i>
+        </div>
+        <a href="{{ route('admin.categories.index') }}" class="small-box-footer">
+          More info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+      </div>
     </div>
-    <!-- /.card-body -->
-    <div class="card-footer">
-      Footer
+    <!-- ./end categories -->
+
+     <!-- Users -->
+    <div class="col-lg-3 col-6">
+      <!-- small card -->
+      <div class="small-box bg-warning">
+        <div class="inner">
+          <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+          <p>Users</p>
+        </div>
+        <div class="icon">
+          <i class="fas fa-users"></i>
+        </div>
+        <a href="#" class="small-box-footer">
+          More info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+      </div>
     </div>
-    <!-- /.card-footer-->
+    <!-- ./end users -->
   </div>
-  <!-- /.card -->
 
 </section>
 <!-- /.content -->

7ea0a93fd9223314d1fd644544049116a7662de3
oleg_remizov <job.rem1zoff@gmail.com>
2024-11-25 14:13:31 +0300

    register

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,7 @@ use App\Http\Controllers\Admin\PostController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 
 Route::prefix('admin')->name('admin.')->group(function() {
@@ -27,9 +28,7 @@ Route::prefix('admin')->name('admin.')->group(function() {
     Route::resource('categories', CategoryController::class);
     Route::resource('tags', TagController::class);
     Route::resource('posts', PostController::class);
-
-
-
-
-
 });
+
+Route::get('/register', [UserController::class, 'create'])->name('register.create');
+Route::post('/register', [UserController::class, 'store'])->name('register.store');
